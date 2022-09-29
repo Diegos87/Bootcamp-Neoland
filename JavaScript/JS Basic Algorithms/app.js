a6f80f918c369756ad5a994dbc19bbe047773a56
Diegos87 <diego.sanchez87@hotmail.com>
2022-09-29 21:19:54 +0200

    interaccion 1

---
+++ JavaScript/JS Basic Algorithms/app.js
@@ -0,0 +1,10 @@
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero);
+let x = "50";
+let h = "5";
+let y = "10";
+let z = "h " + "y";
+console.log(x);
+console.log(h);
+console.log(y);
+console.log(z);
