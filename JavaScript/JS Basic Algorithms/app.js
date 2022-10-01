b84a0ea5f6f0cbd57b2bb60eea92046418994476
Diegos87 <diego.sanchez87@hotmail.com>
2022-10-01 15:37:39 +0200

    interaccion 5

--- JavaScript/JS Basic Algorithms/app.js
+++ JavaScript/JS Basic Algorithms/app.js
@@ -99,18 +99,30 @@ const number1 = 10;
 const number2 = 20;
 const number3 = 2;
 
-if (number3 == 2) {
+if (number2 / number1) {
   console.log("number2 dividido entre number1 es igual a 2");
 }
 
-if (number1 !== 20) {
+if (number1 !== number2) {
   console.log("number1 es estrictamente distinto a number2");
 }
 
-if (number3 != 10) {
+if (number3 != number1) {
   console.log("numer3 es distinto a number1");
 }
 
-if (number3 == 2) {
+if (number3 * 5) {
   console.log("number3 por 5 es igual a number1");
+}
+
+if (number3 * 5 && number1 * 2) {
+  console.log(
+    "number3 por 5 es igual a number1 y number1 por 2 es igual a number2"
+  );
+}
+
+if (number2 / 2 || number1 / 5) {
+  console.log(
+    "number2 entre 2 es igual a number1 o number1 entre 5 es igual a number3"
+  );
 }
