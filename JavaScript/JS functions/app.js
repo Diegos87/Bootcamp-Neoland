7bf7ee5649f032dc22af5795dad0b7fa5054a838
Diegos87 <diego.sanchez87@hotmail.com>
2022-10-06 22:07:40 +0200

    functions

--- JavaScript/JS functions/app.js
+++ JavaScript/JS functions/app.js
@@ -39,12 +39,33 @@ console.log(longestword);
 const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumAll(numbers) {
-  const total = numbers.reduce(
-    (accumulator, numbers) => accumulator + numbers,
-    0
-  );
-  return total;
+  let accumulator = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    accumulator += numbers[i];
+  }
+  return accumulator;
 }
-
 let total = sumAll(numbers);
 console.log(total);
+
+// Iteracion 4
+
+/*const numbers1 = [12, 21, 38, 5, 45, 37, 6];
+function average(numbers1) {
+  const total1 = numbers.reduce(
+    (accumulator, numbers1) => accumulator + numbers1,
+    0
+  );
+  return total1;
+}*/
+
+const numbers1 = [12, 21, 38, 5, 45, 37, 6];
+function average(numbers1) {
+  let accumulator1 = 0;
+  for (let i = 0; i < numbers1.length; i++) {
+    accumulator1 += numbers1[i] / numbers1.length;
+  }
+  return accumulator1;
+}
+let total1 = average(numbers1);
+console.log(total1);
