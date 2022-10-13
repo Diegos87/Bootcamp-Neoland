1f28088528f00e7de7f4150f8fd57a8c72cbc89d
Diegos87 <diego.sanchez87@hotmail.com>
2022-10-13 20:59:25 +0200

    loops

--- JavaScript/JS Loops/app.js
+++ JavaScript/JS Loops/app.js
@@ -79,9 +79,29 @@ const toys = [
 
 let toysNoCat = [];
 
-for (let toy of toys) {
+for (toy of toys) {
   if (!toy.name.includes("gato")) {
     toysNoCat.push(toy);
   }
 }
 console.log(toysNoCat);
+
+// iteracion 7
+
+const toys1 = [
+  { id: 5, name: "Buzz MyYear", sellCount: 10 },
+  { id: 11, name: "Action Woman", sellCount: 24 },
+  { id: 23, name: "Barbie Man", sellCount: 15 },
+  { id: 40, name: "El gato con Guantes", sellCount: 8 },
+  { id: 40, name: "El gato felix", sellCount: 35 },
+];
+
+const popularToys = [];
+
+for (toy1 of toys1) {
+  if (toy1.sellCount > 15) {
+    popularToys.push(toy1);
+  }
+}
+
+console.log(popularToys);
