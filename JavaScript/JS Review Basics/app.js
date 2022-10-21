8edfd5a8b69ee2e603440ac0594d2ed5d8f1bed8
Diegos87 <diego.sanchez87@hotmail.com>
2022-10-21 20:50:02 +0200

    review

--- JavaScript/JS Review Basics/app.js
+++ JavaScript/JS Review Basics/app.js
@@ -26,3 +26,64 @@ for (let movie of movies) {
 }
 
 console.log(newMovies);
+
+//iteracion 2
+
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+let media;
+
+let volumes = [];
+
+for (let user of users) {
+  for (let key in user.favoritesSounds) {
+    volumes.push(user.favoritesSounds[key].volume);
+  }
+}
+
+console.log(volumes);
+
+const total = (todos) => {
+  let acummulator = 0;
+  for (let volume of todos) {
+    acummulator += volume / todos.length;
+  }
+  return acummulator;
+};
+
+media = total(volumes);
+
+console.log(media);
